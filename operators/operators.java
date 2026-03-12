af58a2263da44f049833b8221d4dea9ea061b45f
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-12 22:18:00 +0530

    Create operators.java

---
+++ operators/operators.java
@@ -0,0 +1,22 @@
+class operator {
+public static void main(String[] args) {
+    int num1=24;
+    int num2=4;
+    int result=num1+num2;
+    System.out.println(result);
+
+    int result2=num1-num2;
+    System.out.println(result2);
+
+    int result3=num1*num2;
+    System.out.println(result3);
+
+    int result4=num1/num2;
+    System.out.println(result4);
+ 
+
+     num1 +=2;   //for incrreasing the value of num 1
+     System.out.println(num1);
+}    
+}
+    
