0957b4c7ecc53d9f78d3d91e3a4d72800a8b065c
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-12 22:21:31 +0530

    Create all datatypes.java

---
+++ datatypes/all datatypes.java
@@ -0,0 +1,28 @@
+class datatypes {
+public static void main(String[] args)
+{
+    
+    int num1=9;
+    byte by=123;
+    short sh=558;
+    long l=5854l;
+
+    //float
+    float f=5.8f;
+    double d=5.8;
+ 
+    //character
+    char c='k';
+
+    //boolean
+    boolean b =true; 
+
+     // literals
+double num4=0b101  ;     
+System.out.println(num4);
+int a = 10;     // 10 is a literal
+char ch = 'A';  // 'A' is a literal
+String s = "Hello"; // "Hello" is a literal
+
+}
+}
