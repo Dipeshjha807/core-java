db037ef95213ac024d4699dc69feb52e961c3d18
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-12 22:12:03 +0530

    Create logical operators.java

---
+++ operators/logical operators.java
@@ -0,0 +1,15 @@
+public class logicial_operator {
+    
+    public static void main(String[] args) {
+        
+        int x=7;
+        int y=6;
+        int a=5;
+        int b=9;
+        boolean result = x>y && a>b;  //and operator 
+        System.out.println(result); //here we use not operatpr which''!' sor if the result is true it give false if result is flase it is true or just remove the ! it is normal the 
+ 
+    }
+}
+//        boolean result = x>y || a<b; or operator
+
