061d914b8bfc84d84d2621d725a4f39820adf751
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-13 23:13:27 +0530

    Add files via upload

---
+++ operators/relational_opp.java
@@ -0,0 +1,13 @@
+public class relational_opp {
+    public static void main(String[] args) {
+        int x=6;
+        int y=6;
+       boolean result=x==y;
+      
+        System.out.println(result);
+    }
+    
+}
+ //x!=y; not equal //x<y  x smaller then 
+       //x>y y greater then
+       //x==y x equal y  
