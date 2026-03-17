2d773316485734a4a145acafdab343d928ddc3fd
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-17 08:40:40 +0530

    Create max_min_values.java

---
+++ values/max_min_values.java
@@ -0,0 +1,10 @@
+ class check {
+     public static void main(String[] args) {
+    float floatmin= Float.MIN_VALUE;
+    Float floatmax=Float.MAX_VALUE;
+
+
+System.out.println("float minimum value:"+floatmin);
+System.out.println("float max value:"+floatmax);
+}
+ }                  
