a6377a5fc00a9cd132464c450ac6a122292989fe
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-19 15:59:00 +0530

    Create if_satatement.java

---
+++ conditional_statements/if_satatement.java
@@ -0,0 +1,14 @@
+public class ifstatement {
+    public static void main(String[] args) {
+        
+        int x=124;
+
+        if (x>34)
+        System.out.println("hello");
+
+     
+        
+
+    }
+}
+/// here it was a simple program for the if statement where it check the value of x is greatewr then 34 or not 
