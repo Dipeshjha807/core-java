acbf96ce3ea77ef4535cc445ba521975e7d69eb7
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-20 22:52:44 +0530

    Update comments in ifelse_statement.java
    Corrected comments for clarity on if-else statement usage.

---
+++ conditional_statements/ifelse_statement.java
@@ -0,0 +1,17 @@
+public class if_elsestatement {
+    public static void main(String[] args) {
+        
+        int x=14;
+        int y=18;
+
+        if (x>y)
+        System.out.println("hello"); //there is only one statement if we want multiple ststement so we have to use {} in 
+                                     //if ststement like check the age of a if age is 18 or not like that
+
+
+        else
+
+        System.out.println(y +"lauda lo na mera lo ");
+
+}
+}
