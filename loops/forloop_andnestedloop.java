8650683a8483ebc4bc3a10563e9e3b35ac8ccc00
Dipeshjha807 <dipeshjha799@gmail.com>
2026-03-23 16:35:05 +0530

    Create forloop_andnestedloop.java

---
+++ loops/forloop_andnestedloop.java
@@ -0,0 +1,18 @@
+public class forloop {
+
+    public static void main(String[] args) {
+        
+        for (int i= 1;i<=4;i++)//outer loop 
+         {
+            System.out.println("meri pyari  barsha" + i);
+            for(int j=1;j<=9;j++)//ineer loop and also example of nested loop
+            {
+                System.out.println("   "+( j +8) +"-" +(j+9));
+            }
+
+            
+        } 
+    }
+
+}
+ // here both example for loop and nested for loop is there if you have any doubt feel free to reach
