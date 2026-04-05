631d1ae79b2e367144e13927ce7140afdc624698
Dipeshjha807 <dipeshjha799@gmail.com>
2026-04-05 20:52:55 +0530

    Create array1.java

---
+++ arrays/array1.java
@@ -0,0 +1,13 @@
+public class array1 {
+    public static void main(String[] args) {
+        int n[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        n[1] = 4;   
+        for(int i=0;i<6;i++)
+        {
+
+            System.out.println(i);
+        }
+    }
+
+}
+//it was a simple array question the complex questions is on the way for dsa
