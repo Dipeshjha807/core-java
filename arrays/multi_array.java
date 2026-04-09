a0816c2a8557b79242ce7a05a147b446c2b6ec83
Dipeshjha807 <dipeshjha799@gmail.com>
2026-04-09 20:26:29 +0530

    Add multi_array.java for multi-dimensional array example
    Implement a basic example of a multi-dimensional array in Java.

---
+++ arrays/multi_array.java
@@ -0,0 +1,18 @@
+class multi_array {
+    public static void main(String[] args) {
+        int num[][] = new int[3][4];
+
+         num[0][2]=4;//giving random values by own
+         num[2][1]=5;
+         num[0][0]=23;
+         for(int i=0; i<3;i++){
+            for(int j=0;j<4;j++)
+            {
+                System.out.println(num[i][j]);
+              
+            }
+         }
+        
+        }
+    }
+//it was a basic multi dimentional rray example for learning  just be on the boat we rais the bar like anyrhing
