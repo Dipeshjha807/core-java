83edc85b16b67d144c3f98549efc909aa2bfe934
Dipeshjha807 <dipeshjha799@gmail.com>
2026-06-26 20:55:32 +0530

    Create static_method.java

---
+++ strings/static_method.java
@@ -0,0 +1,9 @@
+public class static_method {
+    static void show()
+    {
+        System.out.println("method is running");
+    }
+    public static void main(String[] args) {
+        show();   // if we call the statice method in the same class then using of class name to call was optional you can uske aur just write method name 
+    }
+}
