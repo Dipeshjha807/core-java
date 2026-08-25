831b820a30ccb960bf2c0f4199a0a69e67d5eeb9
Dipesh Jha <dipeshjha799@gmail.com>
2026-08-25 21:52:32 +0530

    Create Vehicle.java

---
+++ dynamicMethod dispatch/Vehicle.java
@@ -0,0 +1,32 @@
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle starts");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void start() {
+        System.out.println("Car starts with key");
+    }
+}
+
+class Bike extends Vehicle {
+    @Override
+    void start() {
+        System.out.println("Bike starts with self-start");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle v;
+
+        v = new Car();
+        v.start();
+
+        v = new Bike();
+        v.start();
+    }
+}
