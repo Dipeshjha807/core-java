ce18bf544e46bc1b3df86e109bdb2a445fa4dbcf
Dipesh Jha <dipeshjha799@gmail.com>
2026-08-29 22:28:16 +0530

    Create  typepromotion.java

---
+++ constructor/typepromotion.java
@@ -0,0 +1,10 @@
+ class typepromotion {
+    
+    public static void main(String[] args) {
+        byte a=23;
+        byte b =12;
+
+        int result=a*b;    //promating from 8byte ti int and it is typ promotionj
+        System.out.println(result);
+    }
+}
